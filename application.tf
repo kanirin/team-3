2934e522a8b2b10c1a2289a4faaa7be8e2360cda
kanirin <111390977+kanirin@users.noreply.github.com>
2023-02-25 17:33:25 -0600

    Update application.tf

--- application.tf
+++ application.tf
@@ -7,24 +7,26 @@ module "wordpress" {
   deployment_namespace = "team-3"
   deployment_path      = "./application"
   values_yaml          = <<EOF
-ingress:
-  enabled: true
-  className: "nginx"
-  annotations: 
-    cert-manager.io/cluster-issuer: letsencrypt-prod
-    nginx.ingress.kubernetes.io/proxy-body-size: "0"
-    ingress.kubernetes.io/ssl-redirect: "true"
-    acme.cert-manager.io/http01-edit-in-place: "true"
-  hosts:
-    - host: application.rindevops.com
-      paths:
-        - path: /
-          pathType: ImplementationSpecific
-  tls: 
-  - secretName: chart-example-tls
+server:
+  ingress:
+    enabled: true
+    className: "nginx"
+    annotations: 
+      cert-manager.io/cluster-issuer: letsencrypt-prod
+      nginx.ingress.kubernetes.io/proxy-body-size: "0"
+      ingress.kubernetes.io/ssl-redirect: "false"
+      acme.cert-manager.io/http01-edit-in-place: "true"
     hosts:
-      - application.rindevops.com
-    EOF
+    - host: application.rindevops.com
+      http:
+        paths:
+        - path: "/"
+          pathType: ImplementationSpecific
+    tls: 
+      - secretName: chart-example-tls
+        hosts:
+        - application.rindevops.com
+  EOF
 }
 
 
