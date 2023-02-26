54a0f8e6b5777719c0f70a8d156d99834c335efa
DianaMac84 <107662802+DianaMac84@users.noreply.github.com>
2023-02-26 15:44:18 -0600

    updated path

--- Uptime_check.tf
+++ Uptime_check.tf
@@ -1,5 +1,5 @@
 module "uptime_check" {
-  source             = "../modules/monitoring"
+  source             = "./modules/monitoring"
   google_domain_name = var.google_domain_name
   PROJECT_ID         = var.PROJECT_ID
   service_name       = "uptime_check"
