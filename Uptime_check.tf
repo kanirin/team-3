eebe7a8254657bba1da40b5f951e186fec39c54a
DianaMac84 <107662802+DianaMac84@users.noreply.github.com>
2023-02-26 19:22:35 -0600

    fixed uptime-check and created vault

--- Uptime_check.tf
+++ Uptime_check.tf
@@ -1,5 +1,5 @@
 module "uptime_check" {
-  source             = "./modules/monitoring"
+  source             = "./module/monitoring"
   google_domain_name = var.google_domain_name
   PROJECT_ID         = var.PROJECT_ID
   service_name       = "uptime_check"
